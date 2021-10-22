ec3ed6a9202f8474b3da2c2395615c2225d02a80
Onik Noor <O_Noor@live.co.uk>
2021-10-22 19:59:43 +0100

    Initial Core Commit with User Entity

---
+++ Otal.lmaoo.Core/Entities/User/User.cs
@@ -0,0 +1,13 @@
+﻿namespace Otal.lmaoo.Core.Entities.User
+{
+    public class User
+    {
+        public int UserId { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Forename { get; set; }
+        public string Surname { get; set; }
+        public int Level { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
