c38cfc7c28ce94f116a27fa72db5be9d7f87eabb
Onik Noor <O_Noor@live.co.uk>
2021-10-22 19:58:59 +0100

    Initial Database Commit with 'Get' User

---
+++ Otal.lmaoo.Database/dbo/Tables/User.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[User]
+(
+  [UserId] int NOT NULL IDENTITY,
+  [Username] varchar(15) NOT NULL,
+  [Password] varchar(255) NOT NULL,
+  [Forename] varchar(30) NOT NULL,
+  [Surname] varchar(30) NOT NULL,
+  [Level] SMALLINT NOT NULL DEFAULT '1',
+  [IsActive] smallint NOT NULL DEFAULT '1',
+  [Picture] varchar(50) NOT NULL DEFAULT '/Images/avatar.jpg'
+  PRIMARY KEY ([UserId])
+);
