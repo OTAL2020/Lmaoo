5a7459e258e31b79f8ce309079e7315bb45cb4c1
Lewisaaronwelch <48370571+LewisWelch94@users.noreply.github.com>
2021-10-31 07:38:09 +0000

    Entities - Changed variable type from byte to bool

--- Otal.lmaoo.Core/Entities/ProjectAccess.cs
+++ Otal.lmaoo.Core/Entities/ProjectAccess.cs
@@ -5,7 +5,7 @@ namespace Otal.lmaoo.Core.Entities
         public int ProjectAccessId { get; set; }
         public int UserId { get; set; }
         public int ProjectId { get; set; }
-        public byte AllowAccess { get; set; }
-        public byte ManagerAccess { get; set; }
+        public bool AllowAccess { get; set; }
+        public bool ManagerAccess { get; set; }
     }
 }
