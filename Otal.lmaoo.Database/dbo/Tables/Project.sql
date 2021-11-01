28c700a0e956de90f4199184ab65b8ab4f51234d
Onik Noor <O_Noor@live.co.uk>
2021-11-01 21:30:02 +0000

    Added Project.sql with Foreign Key

---
+++ Otal.lmaoo.Database/dbo/Tables/Project.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[Project] 
+(
+  [ProjectId]	INT			NOT NULL	IDENTITY,
+  [Name]		VARCHAR(20) NOT NULL,
+  [Status]		VARCHAR(20) NOT NULL,
+  [Owner]		INT			NOT NULL,
+  [Active]		BIT			NOT NULL	DEFAULT	'1',
+
+  CONSTRAINT [PK_ProjectId]		 PRIMARY KEY ([ProjectId]),
+  CONSTRAINT [FK_Project_UserId] FOREIGN KEY ([owner])		REFERENCES [dbo].[User] ([UserId])
+)
