7f10b84c5e995a9aeab81f5cdf8ab9a8720513eb
Onik Noor <O_Noor@live.co.uk>
2021-11-01 21:29:47 +0000

    Tidied User.sql

--- Otal.lmaoo.Database/dbo/Tables/User.sql
+++ Otal.lmaoo.Database/dbo/Tables/User.sql
@@ -1,12 +1,13 @@
 ﻿CREATE TABLE [dbo].[User]
 (
-  [UserId] int NOT NULL IDENTITY,
-  [Username] varchar(15) NOT NULL,
-  [Password] varchar(255) NOT NULL,
-  [Forename] varchar(30) NOT NULL,
-  [Surname] varchar(30) NOT NULL,
-  [Level] SMALLINT NOT NULL DEFAULT '1',
-  [IsActive] smallint NOT NULL DEFAULT '1',
-  [Picture] varchar(50) NOT NULL DEFAULT '/Images/avatar.jpg'
+  [UserId]		INT				NOT NULL	IDENTITY,
+  [Username]	VARCHAR(15)		NOT NULL,
+  [Password]	VARCHAR(255)	NOT NULL,
+  [Forename]	VARCHAR(30)		NOT NULL,
+  [Surname]		VARCHAR(30)		NOT NULL,
+  [Level]		BIT				NOT NULL	DEFAULT '1',
+  [IsActive]	BIT				NOT NULL	DEFAULT '1',
+  [Picture]		VARCHAR(100)	NOT NULL	DEFAULT '/Images/avatar.jpg'
+
   PRIMARY KEY ([UserId])
 );
