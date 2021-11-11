645d43256e9d165cbcfcc7b4987b8105ee69c430
Lewisaaronwelch <48370571+LewisWelch94@users.noreply.github.com>
2021-11-11 14:44:08 +0000

    Created Comment SQL table

---
+++ Otal.lmaoo.Database/dbo/Tables/Comment.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[Comment] 
+(
+  [commentId]		int			 NOT NULL	IDENTITY,
+  [commentContent]  varchar(255) NOT NULL,
+  [commentCreated]  datetime2	 NOT NULL	DEFAULT GETDATE(),
+  [ticketId]		int DEFAULT  NULL,
+  [userId]			int DEFAULT  NULL,
+
+  PRIMARY KEY ([commentId]),
+  
+  CONSTRAINT [FK_TicketId]  FOREIGN KEY ([TicketId])	REFERENCES [dbo].[Ticket] ([ticketId]),
+  CONSTRAINT [FK_UserId]	FOREIGN KEY ([UserId])		REFERENCES [dbo].[User]  ([userId])
+)
