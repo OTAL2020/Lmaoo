24ecf8e19a7708688716b8dfdc78b9e92c6ef356
Onik Noor <O_Noor@live.co.uk>
2021-11-13 16:47:16 +0000

    Auth - User now Enum Roles

---
+++ Otal.lmaoo.Core/Enums/UserRole.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Otal.lmaoo.Core.Enums
+{
+    public enum UserRole
+    {
+        Developer = 1,
+
+        Manager = 2,
+
+        Admin = 3,
+    }
+}
