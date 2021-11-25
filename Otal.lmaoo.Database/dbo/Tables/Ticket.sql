f569e7ca12f849acb050dc555ec9c5ca06aab4a8
Lewisaaronwelch <48370571+LewisWelch94@users.noreply.github.com>
2021-11-25 13:07:30 +0000

    Table - Fixed Name error

--- Otal.lmaoo.Database/dbo/Tables/Ticket.sql
+++ Otal.lmaoo.Database/dbo/Tables/Ticket.sql
@@ -5,11 +5,11 @@
   [FeatureId]	int				NOT NULL,
   [ReporterKey] int				DEFAULT		NULL,
   [AssigneeKey] int				DEFAULT		NULL,
-  [progress]	varchar(20)		NOT NULL	DEFAULT 'Open',
   [deadline]	timestamp		NULL DEFAULT NULL,
-  PRIMARY KEY ([ticketId]),
   [Created]		datetime		NOT NULL	DEFAULT CURRENT_TIMESTAMP,
   [Updated]		datetime		NOT NULL,
+  [Progress]	varchar(20)		NOT NULL	DEFAULT 'Open',
+  PRIMARY KEY (TicketId),
 
   CONSTRAINT [FK_Ticket_AssigneeKey]	FOREIGN KEY ([Assigneekey])		REFERENCES [dbo].[User] ([userId]),
   CONSTRAINT [FK_Ticket_FeatureKey]		FOREIGN KEY ([featureId])		REFERENCES [dbo].[Feature] ([featureId]),
