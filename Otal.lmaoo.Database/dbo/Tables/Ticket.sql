9899cd072ab9ec47120107c2192b425fff6cfee4
Lewisaaronwelch <48370571+LewisWelch94@users.noreply.github.com>
2021-11-25 13:07:03 +0000

    Table - Code Cleanup

--- Otal.lmaoo.Database/dbo/Tables/Ticket.sql
+++ Otal.lmaoo.Database/dbo/Tables/Ticket.sql
@@ -5,11 +5,11 @@
   [FeatureId]	int				NOT NULL,
   [ReporterKey] int				DEFAULT		NULL,
   [AssigneeKey] int				DEFAULT		NULL,
-  [created]		timestamp		NOT NULL	DEFAULT CURRENT_TIMESTAMP,
-  [updated]		timestamp		NULL		DEFAULT CURRENT_TIMESTAMP,
   [progress]	varchar(20)		NOT NULL	DEFAULT 'Open',
   [deadline]	timestamp		NULL DEFAULT NULL,
   PRIMARY KEY ([ticketId]),
+  [Created]		datetime		NOT NULL	DEFAULT CURRENT_TIMESTAMP,
+  [Updated]		datetime		NOT NULL,
 
   CONSTRAINT [FK_Ticket_AssigneeKey]	FOREIGN KEY ([Assigneekey])		REFERENCES [dbo].[User] ([userId]),
   CONSTRAINT [FK_Ticket_FeatureKey]		FOREIGN KEY ([featureId])		REFERENCES [dbo].[Feature] ([featureId]),
