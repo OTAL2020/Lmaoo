e3aceb7342b1be3045a2576e6824306536027c4e
Lewisaaronwelch <48370571+LewisWelch94@users.noreply.github.com>
2022-01-06 11:42:58 +0000

    Created SQL backend of Admin CRUD

---
+++ Otal.lmaoo.Database/dbo/Stored Procedures/User_DeactivateUser.sql
@@ -0,0 +1,12 @@
+﻿CREATE PROCEDURE [dbo].[User_DeactivateUser]
+	@UserId INT,
+	@IsActive SMALLINT
+AS
+	SET NOCOUNT ON
+
+	UPDATE [User]
+	  SET [IsActive] = @IsActive
+	FROM 
+	  [dbo].[User]
+	WHERE
+	  [UserId] = @UserId
---
+++ Otal.lmaoo.Database/dbo/Stored Procedures/User_GetByactive.sql
@@ -0,0 +1,16 @@
+﻿CREATE PROCEDURE [dbo].[User_GetByactive]
+	@IsActive INT
+AS
+	SET NOCOUNT ON
+
+	SELECT
+	  [UserId],
+	  [Username],
+	  [Forename],
+	  [Surname],
+	  [Level],
+	  [IsActive]
+	FROM 
+	  [dbo].[User]
+	WHERE
+	  [UserId] = @IsActive
