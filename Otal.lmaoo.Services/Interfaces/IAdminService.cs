7917123f82b2decc1aa4a167e858fd3aa5eb28eb
Lewisaaronwelch <48370571+LewisWelch94@users.noreply.github.com>
2022-01-06 11:44:12 +0000

    Created Admin services with interface

---
+++ Otal.lmaoo.Services/Interfaces/IAdminService.cs
@@ -0,0 +1,12 @@
+﻿namespace Otal.lmaoo.Services.Interfaces
+{
+    using Otal.lmaoo.Core.Entities;
+    public interface IAdminService
+    {
+        User GetByIsActive(int id);
+
+        User UpdateUser(string Username, string Forename, string Surname, int Level, bool IsActive);
+
+        User DeactivateUser(bool IsActive);
+    }
+}
---
+++ Otal.lmaoo.Services/ServiceObjects/AdminServices.cs
@@ -0,0 +1,31 @@
+﻿namespace Otal.lmaoo.Services.ServiceObjects
+{
+    using Otal.lmaoo.Core.Entities;
+    using Otal.lmaoo.Data.Interfaces;
+    using Otal.lmaoo.Services.Interfaces;
+    public class AdminService : IAdminService
+    {
+        protected readonly IAdminDao _dao;
+
+        public AdminService(IAdminDao dao)
+        {
+            _dao = dao;
+        }
+
+        public User GetByIsActive(int IsActive)
+        {
+            return _dao.GetByIsActive(IsActive);
+        }
+
+        public User UpdateUser(string Username, string Forename, string Surname, int Level, bool IsActive)
+        {
+            return _dao.UpdateUser(Username,Forename,Surname,Level,IsActive);
+        }
+
+        public User DeactivateUser(bool IsActive)
+        {
+            IsActive = false;
+            return _dao.DeactivateUser(IsActive);
+        }
+    }
+}
