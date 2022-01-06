02cdf5bd74d8d5bf4bb9b887c15be0fc4dee407d
Lewisaaronwelch <48370571+LewisWelch94@users.noreply.github.com>
2022-01-06 11:44:38 +0000

    Added admin view modal

---
+++ Otal.lmaoo.Web/ViewModels/AdminViewModal.cs
@@ -0,0 +1,14 @@
+﻿namespace Otal.lmaoo.Web.ViewModels
+{
+    public class AdminViewModel
+    {
+        public string IsActiveSelect { get; set; }
+        public int UserId { get; set; }
+        public string UserName { get; set; }
+        public string Forename { get; set; }
+        public string Surname { get; set; }
+        public int Level { get; set; }
+        public bool IsActive { get; set; }
+        public string ReturnUrl { get; set; }
+    }
+}
