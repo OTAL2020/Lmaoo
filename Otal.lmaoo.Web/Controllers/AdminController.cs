2aa239ec9fe416bc62401cc2f447e6e27c8900ad
Lewisaaronwelch <48370571+LewisWelch94@users.noreply.github.com>
2022-01-06 11:44:26 +0000

    updated admin controller

--- Otal.lmaoo.Web/Controllers/AdminController.cs
+++ Otal.lmaoo.Web/Controllers/AdminController.cs
@@ -1,12 +1,35 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Otal.lmaoo.Services.Interfaces;
+using Otal.lmaoo.Web.ViewModels;
 
 namespace Otal.lmaoo.Web.Controllers
 {
     public class AdminController : BaseController
     {
+        private readonly IAdminService _adminService;
+
+        public AdminController(IAdminService AdminService)
+        {
+            _adminService = AdminService;
+        }
+
+        [HttpGet]
+        [AllowAnonymous]
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult getUserByIsActive(AdminViewModel vm)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+
+
         }
     }
 }
