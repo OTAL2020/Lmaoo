ec226e6d2fd407db7a1a4f7b4e14c4153160c272
Onik Noor <O_Noor@live.co.uk>
2022-01-16 14:14:25 +0000

    Dao now has Base Interface

---
+++ Otal.lmaoo.Data/Interfaces/IDaoBase.cs
@@ -0,0 +1,13 @@
+﻿namespace Otal.lmaoo.Data.Interfaces
+{
+    public interface IDaoBase<T>
+    {
+        T Create(T TObject);
+
+        T GetById(int id);
+
+        T Update(T TObject, int id);
+
+        void Delete(int id);
+    }
+}
