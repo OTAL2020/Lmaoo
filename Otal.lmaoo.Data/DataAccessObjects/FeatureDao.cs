454d90bfcf50e4ac31f782e4e33f11314a445e3b
Onik Noor <O_Noor@live.co.uk>
2022-01-16 11:40:17 +0000

    FeatureDao -> Implemented

---
+++ Otal.lmaoo.Data/DataAccessObjects/FeatureDao.cs
@@ -0,0 +1,47 @@
+﻿namespace Otal.lmaoo.Data.DataAccessObjects
+{
+    using Dapper;
+    using Microsoft.Extensions.Configuration;
+    using Otal.lmaoo.Core.Entities;
+    using Otal.lmaoo.Data.Interfaces;
+    using System.Collections.Generic;
+    using System.Data;
+    using System.Linq;
+
+    public class FeatureDao : DaoBase, IFeatureDao
+    {
+        private new readonly IConfiguration _configuration;
+
+        public FeatureDao(IConfiguration configuration) : base(configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public Feature GetById(int featureId)
+        {
+            using (var con = NewSqlConnection)
+            {
+                return con.Query<Feature>("[dbo].[Feature_GetById]", new { FeatureId = featureId },
+                    commandType: CommandType.StoredProcedure).SingleOrDefault();
+            }
+        }
+
+        public IEnumerable<Feature> GetActiveByProjectId(int projectId)
+        {
+            using (var con = NewSqlConnection)
+            {
+                return con.Query<Feature>("[dbo].[Feature_GetActiveByProjectId]", new { ProjectId = projectId },
+                    commandType: CommandType.StoredProcedure);
+            }
+        }
+
+        public IEnumerable<Feature> GetInactiveByProjectId(int projectId)
+        {
+            using (var con = NewSqlConnection)
+            {
+                return con.Query<Feature>("[dbo].[Feature_GetInactiveByProjectId]", new { ProjectId = projectId },
+                    commandType: CommandType.StoredProcedure);
+            }
+        }
+    }
+}
---
+++ Otal.lmaoo.Data/Interfaces/IFeatureDao.cs
@@ -0,0 +1,14 @@
+﻿using Otal.lmaoo.Core.Entities;
+using System.Collections.Generic;
+
+namespace Otal.lmaoo.Data.Interfaces
+{
+    public interface IFeatureDao
+    {
+        Feature GetById(int id);
+
+        IEnumerable<Feature> GetActiveByProjectId(int projectId);
+
+        IEnumerable<Feature> GetInactiveByProjectId(int projectId);
+    }
+}
