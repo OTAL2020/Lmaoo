2e26e71fa342f0e53c7a587bbd6673a60cf83148
Onik Noor <O_Noor@live.co.uk>
2022-01-16 15:29:52 +0000

    Dao -> Interfaces are all done, also added some changes Owner to OwnerId

---
+++ Otal.lmaoo.Data/Interfaces/IProjectDao.cs
@@ -0,0 +1,19 @@
+﻿namespace Otal.lmaoo.Data.Interfaces
+{
+    using Otal.lmaoo.Core.Entities;
+    using Otal.lmaoo.Data.Interfaces.Base;
+    using System.Collections.Generic;
+
+    public interface IProjectDao : IDaoBase<Ticket>
+    {
+        IEnumerable<Project> GetByOwnerId(int userId);
+
+        void CreateAccess(IEnumerable<ProjectAccess> projectAccesses);
+
+        IEnumerable<Project> GetByStandardAccess(int projectId);
+
+        IEnumerable<Project> GetByManagerAccess(int projectId);
+
+        void DeleteAccess(int projectId);
+    }
+}
--- Otal.lmaoo.Database/dbo/Tables/Project.sql
+++ Otal.lmaoo.Database/dbo/Tables/Project.sql
@@ -3,9 +3,9 @@
   [ProjectId]	INT			NOT NULL	IDENTITY,
   [Name]		VARCHAR(20) NOT NULL,
   [Status]		VARCHAR(20) NOT NULL,
-  [Owner]		INT			NOT NULL,
+  [OwnerId]		INT			NOT NULL,
   [Active]		BIT			NOT NULL	DEFAULT	('1'),
 
   CONSTRAINT [PK_Project_ProjectId]		PRIMARY KEY ([ProjectId]),
-  CONSTRAINT [FK_Project_UserId]		FOREIGN KEY ([Owner])		REFERENCES [dbo].[User] ([UserId])
+  CONSTRAINT [FK_Project_UserId]		FOREIGN KEY ([OwnerId])		REFERENCES [dbo].[User] ([UserId])
 )
