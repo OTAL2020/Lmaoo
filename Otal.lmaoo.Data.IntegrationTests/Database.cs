52aef86f9582896be5a7bdac1b654e2a6a72717d
Onik Noor <O_Noor@live.co.uk>
2022-01-28 23:46:32 +0000

    IntegrationImplementation - Database User Prep

---
+++ Otal.lmaoo.Data.IntegrationTests/Database.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Data.SqlClient;
+using Xunit.Abstractions;
+
+namespace Otal.lmaoo.Data.IntegrationTests
+{
+    public static class Database
+    {
+        public static string ConnectionString()
+        {
+            var db_db = Environment.GetEnvironmentVariable("DB_DATABASE");
+            var db_pass = Environment.GetEnvironmentVariable("DB_PASSWORD");
+            var connectionString = string.Format("Data Source=db;Database={0};User Id=sa;Password={1};", db_db, db_pass);
+
+            return connectionString;
+        }
+
+        public static bool CheckDatabaseExists(string connectionString, string databaseName)
+        {
+            using (var connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                using (var command = new SqlCommand($"SELECT db_id('{databaseName}')", connection))
+                {
+                    return (command.ExecuteScalar() != DBNull.Value);
+                }
+            }
+        }
+
+        public static int RunQuery(string connectionString, string query)
+        {
+            using (var connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                using (var command = new SqlCommand($"{query}", connection))
+                {
+                    return command.ExecuteNonQuery();
+                }
+            }
+        }
+    }
+}
