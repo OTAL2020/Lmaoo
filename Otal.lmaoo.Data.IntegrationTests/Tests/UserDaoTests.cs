c73ca2e6a265aab3d91df0912bc9aaba1a3cde02
Onik Noor <O_Noor@live.co.uk>
2022-01-28 23:47:54 +0000

    IntegrationImplementation - Get User by Id Implemented

---
+++ Otal.lmaoo.Data.IntegrationTests/Tests/UserDaoTests.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Extensions.Configuration;
+using Otal.lmaoo.Data.DataAccessObjects;
+using System.IO;
+using Xunit;
+
+namespace Otal.lmaoo.Data.IntegrationTests.Tests
+{
+    public class UserDaoTests : IClassFixture<DatabaseFixture>
+    {
+        private readonly DatabaseFixture _databaseFixture;
+        private readonly IConfiguration _configuration;
+        private readonly UserDao _userDao;
+
+        public UserDaoTests(DatabaseFixture fixture)
+        {
+            _databaseFixture = fixture;
+
+             _configuration = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile(
+                 path: "appsettings.json",
+                 optional: false,
+                 reloadOnChange: true)
+            .Build();
+
+            _configuration["DB_DATABASE"] = "Otal.lmaoo.Database";
+            _configuration["DB_PASSWORD"] = "IntegrationTest123!";
+            
+            _userDao = new UserDao(_configuration);
+        }
+
+        [Fact]
+        public void User_GetUserByUserId()
+        {
+            var user = _userDao.GetById(1);
+
+            Assert.NotNull(user);
+            Assert.Equal(1, user.UserId);
+        }
+    }
+}
