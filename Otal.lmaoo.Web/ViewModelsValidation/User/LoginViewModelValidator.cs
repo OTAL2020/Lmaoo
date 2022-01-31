05d2bcf1041b7e770d806bce890b040f59ab0b93
Onik Noor <O_Noor@live.co.uk>
2022-01-31 15:06:38 +0000

    FluentValidationImplementation - Login Page now uses Fluent Validation

---
+++ Otal.lmaoo.Web/ViewModelsValidation/User/LoginViewModelValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+using Otal.lmaoo.Web.ViewModels.User;
+
+namespace Otal.lmaoo.Web.ViewModelsValidation.User
+{
+    public class LoginViewModelValidator : AbstractValidator<LoginViewModel>
+    {
+        public LoginViewModelValidator()
+        {
+            RuleFor(x => x.Username).NotEmpty().WithMessage("Username is required");
+            RuleFor(x => x.Password).NotEmpty().WithMessage("Password is required");
+        }
+    }
+}
