89f28c94c5ea80c7b0d8b2969ececd0133df38e6
Onik <18009030+OnikNoor97@users.noreply.github.com>
2022-02-03 00:08:38 +0000

    InTestCleanup - Added Powershell script for future

---
+++ Otal.lmaoo.Data.IntegrationTests/setupdatabase.ps1
@@ -0,0 +1,7 @@
+﻿$currentDirectory = Get-Location
+$SQLServer = "localhost"
+$db3 = "master"
+
+Write-Output $currentDirectory
+
+Invoke-Sqlcmd -ServerInstance $SQLServer -Username "SA" -Password "IntegrationTest123!" -Database $db3 -InputFile "$($currentDirectory)\Otal.lmaoo.Database_Create.sql"
