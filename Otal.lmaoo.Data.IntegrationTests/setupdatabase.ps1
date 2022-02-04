7976c103cb26f2d9ccc4e23c95f368b94124d722
Onik Noor <O_Noor@live.co.uk>
2022-02-04 12:42:43 +0000

    PsCleanup - Added Variables

--- Otal.lmaoo.Data.IntegrationTests/setupdatabase.ps1
+++ Otal.lmaoo.Data.IntegrationTests/setupdatabase.ps1
@@ -1,6 +1,10 @@
-﻿$currentDirectory = Get-Location
-$SQLServer = "localhost"
+﻿$SQLServer = "db"
+$Username = "SA"
+$Password = "IntegrationTest123!"
 $db3 = "master"
+$currentDirectory = Get-Location
+$scriptName = "Otal.lmaoo.Database_Create.sql"
+$script = "$currentDirectory\$scriptName"
 
-#Invoke-Sqlcmd -ServerInstance $SQLServer -Username "SA" -Password "IntegrationTest123!" -Database $db3 -InputFile "$($currentDirectory)\Otal.lmaoo.Database_Create.sql"
-#Invoke-Sqlcmd -ServerInstance $SQLServer -Database $db3 -InputFile "$($currentDirectory)\Otal.lmaoo.Database_Create.sql"
+#Invoke-Sqlcmd -ServerInstance $SQLServer -Username $Username -Password $Password -Database $db3 -InputFile $script
+#Invoke-Sqlcmd -ServerInstance $SQLServer -Database $db3 -InputFile $script
