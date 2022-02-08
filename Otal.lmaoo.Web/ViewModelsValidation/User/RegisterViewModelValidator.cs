528e223b45d99843f7717e79dcaadf27eb4c53d5
abdultaibi98 <97127315+abdultaibi98@users.noreply.github.com>
2022-02-08 13:26:05 +0000

    RegisterViewModelValidation implemented

---
+++ Otal.lmaoo.Web/ViewModelsValidation/User/RegisterViewModelValidator.cs
@@ -0,0 +1,21 @@
+﻿namespace Otal.lmaoo.Web.ViewModelsValidation.User
+{
+    using FluentValidation;
+    using Otal.lmaoo.Web.ViewModels.User;
+    using System;
+
+    public class RegisterViewModelValidator : AbstractValidator<RegisterViewModel>
+    {
+        public RegisterViewModelValidator()
+        {
+            RuleFor(x => x.Forename).NotEmpty().WithMessage("Forename is required");
+            RuleFor(x => x.Surname).NotEmpty().WithMessage("Surname is required");
+            RuleFor(x => x.Username).NotEmpty().WithMessage("Username is required");
+            RuleFor(x => x.Password).NotEmpty().WithMessage("Password is required");
+            RuleFor(x => x.Password).Must((x, confirmEmailAddress) => x.ConfirmPassword.Equals(confirmEmailAddress, StringComparison.OrdinalIgnoreCase))
+                                    .WithMessage("Password and Confirmation Password must match");
+            RuleFor(x => x.ConfirmPassword).NotEmpty().WithMessage("Confirm Password is required");
+
+        }
+    }
+}
