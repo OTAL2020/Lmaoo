5c31e643f02adeb98d519dc0642985306188bfa8
abdultaibi98 <97127315+abdultaibi98@users.noreply.github.com>
2022-02-08 13:33:14 +0000

    RegisterViewModelValidation implemented (#23)
    * RegisterViewModelValidation implemented
    
    * Update RegisterViewModelValidator.cs

--- Otal.lmaoo.Web/ViewModels/User/RegisterViewModel.cs
+++ Otal.lmaoo.Web/ViewModels/User/RegisterViewModel.cs
@@ -1,23 +1,15 @@
 ﻿namespace Otal.lmaoo.Web.ViewModels.User
 {
-    using System.ComponentModel.DataAnnotations;
-
     public class RegisterViewModel
     {
-        [Required(ErrorMessage = "Forname is required")]
         public string Forename { get; set; }
 
-        [Required(ErrorMessage = "Surname is required")]
         public string Surname { get; set; }
 
-        [Required(ErrorMessage = "Username is required")]
         public string Username { get; set; }
 
-        [Required(ErrorMessage = "Password is required")]
         public string Password { get; set; }
 
-        [Required(ErrorMessage = "Confirm Password is required")]
-        [Compare("Password", ErrorMessage = "Password and Confirmation Password must match.")]
         public string ConfirmPassword { get; set; }
     }
 }
