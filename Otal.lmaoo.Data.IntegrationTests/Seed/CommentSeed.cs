dec463d220935827263847a0dd5a8873c24ad0f6
Onik Noor <O_Noor@live.co.uk>
2022-03-01 11:58:26 +0000

    CommentDaoTests - Implemented

--- Otal.lmaoo.Data.IntegrationTests/Seed/CommentSeed.cs
+++ Otal.lmaoo.Data.IntegrationTests/Seed/CommentSeed.cs
@@ -14,6 +14,22 @@
             UserId = UserSeed.DeveloperUser.UserId
         };
 
+        public static Comment ToBeUpdatedComment = new Comment
+        {
+            CommentId = 2,
+            Content = "ToBeUpdatedComment",
+            TicketId = TicketSeed.InDevTicket.TicketId,
+            UserId = UserSeed.ManagerUser.UserId
+        };
+
+        public static Comment ToBeDeletedComment = new Comment
+        {
+            CommentId = 3,
+            Content = "ToBeDeletedComment",
+            TicketId = TicketSeed.InDevTicket.TicketId,
+            UserId = UserSeed.ManagerUser.UserId
+        };
+
         public string DataType() => "Comment";
 
         public int OrderNumber() => 5;
@@ -33,6 +49,18 @@
                         BacklogComment.TicketId,
                         BacklogComment.UserId,
                     },
+                    new object[]
+                    {
+                        ToBeUpdatedComment.Content,
+                        ToBeUpdatedComment.TicketId,
+                        ToBeUpdatedComment.UserId,
+                    },
+                    new object[]
+                    {
+                        ToBeDeletedComment.Content,
+                        ToBeDeletedComment.TicketId,
+                        ToBeDeletedComment.UserId,
+                    }
                 });
 
             var compiler = new SqlServerCompiler();
---
+++ Otal.lmaoo.Data.IntegrationTests/Tests/CommentDaoTests.cs
@@ -0,0 +1,111 @@
+﻿namespace Otal.lmaoo.Data.IntegrationTests.Tests
+{
+    using Microsoft.Extensions.Configuration;
+    using Otal.lmaoo.Core.Entities;
+    using Otal.lmaoo.Data.DataAccessObjects;
+    using Otal.lmaoo.Data.IntegrationTests.Seed;
+    using System.IO;
+    using System.Linq;
+    using Xunit;
+    public class CommentDaoTests : IClassFixture<DatabaseFixture>
+    {
+        private readonly DatabaseFixture _databaseFixture;
+        private readonly IConfiguration _configuration;
+        private readonly CommentDao _commentDao;
+
+        public CommentDaoTests(DatabaseFixture fixture)
+        {
+            _databaseFixture = fixture;
+
+            _configuration = new ConfigurationBuilder()
+           .SetBasePath(Directory.GetCurrentDirectory())
+           .AddJsonFile(
+                path: "appsettings.json",
+                optional: false,
+                reloadOnChange: true)
+           .Build();
+
+            _configuration["DB_SOURCE"] = Database.SOURCE;
+            _configuration["DB_PORT"] = Database.PORT;
+            _configuration["DB_DATABASE"] = Database.NAME;
+            _configuration["DB_PASSWORD"] = Database.PASSWORD;
+
+            _commentDao = new CommentDao(_configuration);
+        }
+
+        [Fact]
+        public void Create_ValidComment()
+        {
+            var newComment = new Comment
+            {
+                Content = "New Comment",
+                TicketId = TicketSeed.BackLogTicket.TicketId,
+                UserId = UserSeed.DeveloperUser.UserId,
+            };
+
+            var actualComment = _commentDao.Create(newComment);
+
+            Assert.Equal(newComment.Content, actualComment.Content);
+            Assert.Equal(newComment.TicketId, actualComment.TicketId);
+            Assert.Equal(newComment.UserId, actualComment.UserId);
+            Assert.Equal(UserSeed.DeveloperUser.Forename, actualComment.UserForename);
+            Assert.Equal(UserSeed.DeveloperUser.Surname, actualComment.UserSurname);
+            Assert.Equal(UserSeed.DEFAULT_PICTURE_URL, actualComment.UserPicture);
+        }
+
+        [Fact]
+        public void GetById_ValidComment()
+        {
+            var actualComment = _commentDao.GetById(CommentSeed.BacklogComment.CommentId);
+
+            Assert.Equal(CommentSeed.BacklogComment.Content, actualComment.Content);
+            Assert.Equal(CommentSeed.BacklogComment.TicketId, actualComment.TicketId);
+            Assert.Equal(CommentSeed.BacklogComment.UserId, actualComment.UserId);
+            Assert.Equal(UserSeed.DeveloperUser.Forename, actualComment.UserForename);
+            Assert.Equal(UserSeed.DeveloperUser.Surname, actualComment.UserSurname);
+            Assert.Equal(UserSeed.DEFAULT_PICTURE_URL, actualComment.UserPicture);
+        }
+
+        [Fact]
+        public void GetByTicketId_ValidComment()
+        {
+            var actualComment = _commentDao.GetByTicketId(CommentSeed.BacklogComment.TicketId).First();
+
+            Assert.Equal(CommentSeed.BacklogComment.Content, actualComment.Content);
+            Assert.Equal(CommentSeed.BacklogComment.TicketId, actualComment.TicketId);
+            Assert.Equal(CommentSeed.BacklogComment.UserId, actualComment.UserId);
+            Assert.Equal(UserSeed.DeveloperUser.Forename, actualComment.UserForename);
+            Assert.Equal(UserSeed.DeveloperUser.Surname, actualComment.UserSurname);
+            Assert.Equal(UserSeed.DEFAULT_PICTURE_URL, actualComment.UserPicture);
+        }
+
+        [Fact]
+        public void Update_ValidComment()
+        {
+            var updateComment = new Comment
+            {
+                CommentId = CommentSeed.ToBeUpdatedComment.CommentId,
+                Content = "This comment has now been updated",
+                TicketId = TicketSeed.InDevTicket.TicketId,
+                UserId = UserSeed.ManagerUser.UserId
+            };
+
+            var actualComment = _commentDao.Update(updateComment);
+
+            Assert.Equal(updateComment.Content, actualComment.Content);
+            Assert.Equal(updateComment.TicketId, actualComment.TicketId);
+            Assert.Equal(updateComment.UserId, actualComment.UserId);
+            Assert.Equal(UserSeed.DeveloperUser.Forename, actualComment.UserForename);
+            Assert.Equal(UserSeed.DeveloperUser.Surname, actualComment.UserSurname);
+            Assert.Equal(UserSeed.DEFAULT_PICTURE_URL, actualComment.UserPicture);
+        }
+
+        [Fact]
+        public void Delete_ValidComment()
+        {
+            _commentDao.Delete(CommentSeed.ToBeDeletedComment.CommentId);
+            var deletedComment = _commentDao.GetById(CommentSeed.ToBeDeletedComment.CommentId);
+            Assert.Null(deletedComment);
+        }
+    }
+}
