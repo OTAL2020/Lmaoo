5932ac5bde80b3e8d18506adea4e2c789f0fe273
Lewisaaronwelch <48370571+LewisWelch94@users.noreply.github.com>
2022-03-04 15:17:17 +0000

    Comment-Service - ICommentService
    Created Comment Service Interface

---
+++ Otal.lmaoo.Service/Interfaces/ICommentService.cs
@@ -0,0 +1,12 @@
+﻿namespace Otal.lmaoo.Service.Interfaces
+{
+    using Otal.lmaoo.Core.Entities;
+    using System.Collections.Generic;
+
+    public interface ICommentService
+    {
+        Comment CreateComment(Comment comment);
+        Comment GetCommentById(int commentId);
+        IEnumerable <Comment> GetCommentByTicketId(int ticketId);
+    }
+}
