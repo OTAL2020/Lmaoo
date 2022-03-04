a6087e86816c5a3314ade1b316cdbd1b918dbb87
Lewisaaronwelch <48370571+LewisWelch94@users.noreply.github.com>
2022-03-04 15:18:01 +0000

    Comment-Service - CommentService
    Created CommentService and added in base functions

---
+++ Otal.lmaoo.Service/ServiceObjects/CommentService.cs
@@ -0,0 +1,37 @@
+﻿namespace Otal.lmaoo.Service.ServiceObjects
+{
+    using Otal.lmaoo.Core.Entities;
+    using Otal.lmaoo.Data.Interfaces;
+    using Otal.lmaoo.Service.Interfaces;
+    using System.Collections.Generic;
+
+    public class CommentService : ICommentService
+    {
+        protected readonly ICommentDao _commentDao;
+
+        public CommentService(ICommentDao commentDao)
+        {
+            _commentDao = commentDao;
+        }
+
+        public Comment CreateComment(Comment comment)
+        {
+             return _commentDao.Create(comment);
+        }
+
+        public Comment GetCommentById(int commentId)
+        {
+            return _commentDao.GetById(commentId);
+        }
+
+        public IEnumerable<Comment> GetCommentByTicketId(int TicketId)
+        {
+            return _commentDao.GetByTicketId(TicketId);
+        }
+
+        public Comment UpdateComment(Comment comment)
+        {
+            return _commentDao.Update(comment);
+        }
+    }
+}
